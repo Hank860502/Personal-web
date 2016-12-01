85d3c72b5ff243a3c3b0ef1b2553be9b9d5d11ba
Hank860502 <hank860502@gmail.com>
2016-12-01 23:35:31 +0800

    fix bug-click spanIntro will hide techused p tag, add another class on toggle targets

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -85,7 +85,7 @@ function linkReturnsColor(e){
 
 function spanIntro(e){
 	e.preventDefault();
-	$("p").toggle();
+	$("p.light.intro").toggle();
 	$("a.more_text").toggle();
 	if($(".read_more").find('i.fa.fa-angle-double-down')[0]){
 		$(".col.s12.center.button_toggle").toggle()
