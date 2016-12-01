0e2cbd66f7f2d2db1efb5e512a20e2c0821239fd
Hank860502 <hank860502@gmail.com>
2016-12-01 11:21:03 +0800

    itf profile link toggle with text

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -37,23 +37,11 @@ function linkReturnsColor(e){
 function spanIntro(e){
 	e.preventDefault();
 	$("p").toggle();
+	$("a.more_text").toggle();
 	if($(".read_more").find('i.fa.fa-angle-double-down')[0]){
-		$(".col.s12.center.button_toggle").toggle()} 
-	else { 
-		"replace icon with fa-angle-double-down"
-	}
-	// if($(".read_more").html()==="")
+		$(".col.s12.center.button_toggle").toggle()
+	} 
 }
-	// $(".more_text").css({"display": "inline"});
-	// $(".original_text").css({"display": "none"})
-	// $(".read_more").replaceWith("<a class='black-text read_less' href='#'><i aria-hidden='true' class='fa fa-angle-double-up fa-3x'></i></a>")
-
-// function hideIntro(e){
-// 	e.preventDefault();
-// 	$(".more_text").css({"display": "none"});
-// 	$(".original_text").css({"display": "inline"})
-// 	$(this).children().replaceWith("<a class='black-text read_more' href='#'><i aria-hidden='true' class='fa fa-angle-double-down fa-3x'></i></a>")
-// }
 
 function newWindow(e){
 	e.preventDefault();
