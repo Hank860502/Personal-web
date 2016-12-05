d968083d653183cc3b6ac1fa0aafc997f3b7a70f
Hank860502 <hank860502@gmail.com>
2016-12-06 00:51:30 +0800

    add RSpec into tech chart

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -21,7 +21,7 @@ $(document).ready(function() {
         onmouseout: function (d, i) { console.log("onmouseout", d, i); }
 	  },
 	  color: {
-	  	pattern: ["#E77400", "#E70000", "#0074E7", "#e7e700", "#00e700", "#74e700", "#68cf00"]
+	  	pattern: ["#E77400", "#E70000", "#0074E7", "#e7e700", "#00e700", "#74e700", "#68cf00", "#E70000"]
 	  },
     donut: {
         title: "TECHNOLOGIES"
@@ -49,6 +49,7 @@ $(document).ready(function() {
 	            ["jQuery", 5],
 	            ["D3.js", 5],
 	            ["C3.js", 5],
+	            ["RSpec", 10]
 	        ]
 	    	});
     	} },
