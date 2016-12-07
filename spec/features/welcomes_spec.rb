bb691e8a8de603281c1a26ea84681cb535a5d99b
Hank860502 <hank860502@gmail.com>
2016-12-07 16:13:17 +0800

    delete all vist root_path in spec and use before(:each) do method

--- spec/features/welcomes_spec.rb
+++ spec/features/welcomes_spec.rb
@@ -12,25 +12,32 @@ RSpec.feature "Welcomes", type: :feature, js: true do
 		end
 	end
 
+	before(:each) do
+    visit root_path
+  end
+
 	context 'has read more button that toggles text' do
 		it 'clicks on read more button will apn hidden text' do
-			visit root_path
 			find(".read_more").find(".fa-3x").click
 			expect(page).to have_content("In August, 2015, I attend my first Hackathon in National Taiwan University, where my interest in computer starts. Learning 3 computer languages, 13 frameworks for web and mobile application development, and 2 test-driven delvelopment, I graduated from Dev Bootcamp in San Francisco during my gap year. Attending big and small competition, I persue my interest in programing and develop my skills in web and mobile development. With my talent in web and mobile application development, my motivation to keep improving is to produce tools that enhance the quality of lives.")
 		end
 		it 'collapse intro when click on read more button twice' do
-			visit root_path
 			find(".read_more").find(".fa-3x").click
 			find(".read_more").find(".fa-3x").click
 			expect(page).to have_no_content("In August, 2015, I attend my first Hackathon in National Taiwan University, where my interest in computer starts. Learning 3 computer languages, 13 frameworks for web and mobile application development, and 2 test-driven delvelopment, I graduated from Dev Bootcamp in San Francisco during my gap year. Attending big and small competition, I persue my interest in programing and develop my skills in web and mobile development. With my talent in web and mobile application development, my motivation to keep improving is to produce tools that enhance the quality of lives.")
 		end
 
+	end
+
+	context 'links in the page that do not redirect' do 
 		it 'scroll back to the top when clicking on toTop button' do
 			visit root_path
 			find(".go_top").click
 			page.should visit("#top")
 		end
 
-	end
+		# it 'changes pictures on sliders' do
 
+		# end
+	end
 end
