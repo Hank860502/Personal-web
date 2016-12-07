07e0283cc9abfeedd57bfe80343e89b7f4fd91b6
Hank860502 <hank860502@gmail.com>
2016-12-07 17:54:21 +0800

    add new tests for slider, generates routes developer routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   resources :athlete
+  resources :developer
 
   root 'welcome#index'
 
--- spec/features/welcomes_spec.rb
+++ spec/features/welcomes_spec.rb
@@ -1,26 +1,28 @@
 require 'rails_helper'
 
 RSpec.feature "Welcomes", type: :feature, js: true do
-	context 'a guest user' do
-		it 'can see the page title' do
-			visit root_path
-		end
-		
-		it 'has my three role' do
-			visit root_path
+
+
+	describe 'a guest user' do		
+		before(:each) do
+	    visit root_path
+	  end
+
+		it 'see content on first page' do
 			expect(page).to have_content("Data Visualization")
 		end
 	end
 
-	before(:each) do
-    visit root_path
-  end
+	describe 'has read more button that toggles text' do
+		before(:each) do
+    	visit root_path
+  	end
 
-	context 'has read more button that toggles text' do
 		it 'clicks on read more button will apn hidden text' do
 			find(".read_more").find(".fa-3x").click
 			expect(page).to have_content("In August, 2015, I attend my first Hackathon in National Taiwan University, where my interest in computer starts. Learning 3 computer languages, 13 frameworks for web and mobile application development, and 2 test-driven delvelopment, I graduated from Dev Bootcamp in San Francisco during my gap year. Attending big and small competition, I persue my interest in programing and develop my skills in web and mobile development. With my talent in web and mobile application development, my motivation to keep improving is to produce tools that enhance the quality of lives.")
 		end
+
 		it 'collapse intro when click on read more button twice' do
 			find(".read_more").find(".fa-3x").click
 			find(".read_more").find(".fa-3x").click
@@ -29,15 +31,32 @@ RSpec.feature "Welcomes", type: :feature, js: true do
 
 	end
 
-	context 'links in the page that do not redirect' do 
+	describe 'links in the page that do not redirect' do 
+		before(:each) do
+    	visit root_path
+  	end
+		
 		it 'scroll back to the top when clicking on toTop button' do
-			visit root_path
 			find(".go_top").click
 			page.should visit("#top")
+			# not sure if visit can be use like this, test passes no matter is("#top") or ("#bottom")
 		end
 
-		# it 'changes pictures on sliders' do
+		it 'changes the current image appearing' do
+			sleep(5)
+			expect(page).to have_content '"Do. Or do not. There is no try." - Yoda, Jedi Master.'
+		end
 
-		# end
+	end
+
+	describe 'links in page that redirects' do
+		before(:each) do
+    	visit root_path
+  	end
+
+		it 'visit developer page' do 
+			first("img").click
+			page.should visit("/developer")
+		end
 	end
 end
